83717cff5c762397ad3967f5ba7b73c1ab24a5f6
pravin23devops <pravin23devops>
2025-04-19 21:19:16 +0530

    vm commit

---
+++ vm_creation/first_ec2.tf
@@ -0,0 +1,15 @@
+# provider "aws" {
+#   region     = "ap-south-1"
+#   access_key = "mention your key"
+#   secret_key = "mention your key"
+# }
+
+resource "aws_instance" "myec2" {
+    ami = "ami-0f1dcc636b69a6438"
+    instance_type = "t2.micro"
+
+    # tags = {
+    #     Name = "my-first-ec2"
+    # }
+  
+}
