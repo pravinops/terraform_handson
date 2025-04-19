f105af49ad50764d356cb462b88bfad35659a1ca
pravin23devops <pravin23devops>
2025-04-19 21:47:42 +0530

    2

--- 2ndproject/readme.md
+++ 2ndproject/readme.md
@@ -1,3 +1,3 @@
 # DESCRIPTION
 
-*** pravin ***
+** pravin **
--- vm_creation/first_ec2.tf
+++ vm_creation/first_ec2.tf
@@ -4,7 +4,7 @@
 #   secret_key = "mention your key"
 # }
 
-resource "aws_instance" "myec2" {
+resource "aws_instance" "my-ec2" {
     ami = "ami-0f1dcc636b69a6438"
     instance_type = "t2.micro"
 
